237383f3836c8d171b826855e80a19a4b70ac2c8
Sumit Chhonker <sumit@vizexperts.com>
2018-04-04 23:48:30 +0530

    Practice Manager Created

---
+++ src/main/java/com/github/chhonker/javautils/chhonkerjava/managers/PracticeManager.java
@@ -0,0 +1,7 @@
+package com.github.chhonker.javautils.chhonkerjava.managers;
+
+public class PracticeManager {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/github/chhonker/javautils/chhonkerjava/repository/PracticeRepository.java
@@ -0,0 +1,4 @@
+package com.github.chhonker.javautils.chhonkerjava.repository;
+
+public class PracticeRepository {
+}
