50f2d720f4e6fd9ca06583e9fe84552f1dd60ced
Sumit Chhonker <sumit@vizexperts.com>
2018-04-04 23:39:19 +0530

    Initial Commit

---
+++ src/main/java/com/github/chhonker/javautils/chhonkerjava/ChhonkerJavaApplication.java
@@ -0,0 +1,12 @@
+package com.github.chhonker.javautils.chhonkerjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChhonkerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChhonkerJavaApplication.class, args);
+	}
+}
